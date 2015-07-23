84de6dcf5ffb9a510519309c5aba186f892d3eb6
Sibo <oyu-gio@hotmail.com>
2015-07-22 21:11:02 -0500

    Initial Commit
    Initial Commit

--- Multi View Segway/ViewController.swift
+++ Multi View Segway/ViewController.swift
@@ -8,11 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet var userInput: UITextField!
+    @IBOutlet var textOutput: UILabel!
 
+    @IBAction func buttonClick(sender: AnyObject) {
+        if(userInput.text != nil && !userInput.text.isEmpty) {
+            textOutput.text = userInput.text;
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.userInput.delegate = self;
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        self.view.endEditing(true);
+    }
 
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder();
+        return true;
+    }
 }
 
